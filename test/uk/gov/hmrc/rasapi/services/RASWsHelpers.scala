44812b4fb1d637bd7b8e8e176ac0a3a37455cb60
Eric Ohwotu <ericohwotu@users.noreply.github.com>
2020-02-10 17:41:22 +0000

    Revert "Ddcnl 1221 (#170)"
    This reverts commit 19caff526e617899d6e8ab75524166139f374bcb.

--- test/uk/gov/hmrc/rasapi/services/RASWsHelpers.scala
+++ test/uk/gov/hmrc/rasapi/services/RASWsHelpers.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2019 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,31 +19,29 @@ package uk.gov.hmrc.rasapi.services
 import java.nio.file.Paths
 
 import mockws.MockWS
-import play.api.libs.Files
 import play.api.mvc._
 import play.api.test.Helpers._
+
 import scala.io.Source
 
-trait RASWsHelpers extends Controller {
-  val ws = MockWS {
+trait RASWsHelpers extends Controller{
+ val ws = MockWS {
     case (POST, "/") => upload
   }
 
-  def upload: Action[MultipartFormData[Files.TemporaryFile]] =
-    Action(parse.multipartFormData) { request =>
-      request.body
-        .file("rasFileKey")
-        .map { file =>
-          // only get the last part of the filename
-          // otherwise someone can send a path like ../../home/foo/bar.txt to write to other files on the system
-          val filename = Paths.get(file.filename).getFileName
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body.file("rasFileKey").map { file =>
 
-          val lines = Source.fromFile(filename.toFile).getLines.toArray
+      // only get the last part of the filename
+      // otherwise someone can send a path like ../../home/foo/bar.txt to write to other files on the system
+      val filename = Paths.get(file.filename).getFileName
 
-          Ok("File uploaded")
-        }
-        .getOrElse {
-          Ok("ERROR")
-        }
+      val lines = Source.fromFile(filename.toFile).getLines.toArray
+
+      Ok("File uploaded")
+    }.getOrElse {
+      Ok("ERROR")
     }
+  }
 }
+
