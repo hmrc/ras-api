c08f21314378eadd26571a8fb536b60bf0336772
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2026-01-07 14:03:27 +0000

    DDCE-7814 Align License Headers for RAS Service Repos

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoVersion = "2.11.0"
-  private val bootstrapVersion = "10.4.0"
+  private val bootstrapVersion = "10.5.0"
 
   private val compile: Seq[ModuleID] = Seq(
     ws,
