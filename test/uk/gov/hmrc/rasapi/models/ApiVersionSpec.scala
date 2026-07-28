35154fb14f5cc774913210dac773349ea81f9977
karishmacharlwood-hmrc <295779981+karishmacharlwood-hmrc@users.noreply.github.com>
2026-07-28 17:02:15 +0100

    DDCE-8959 Add unit test for ApiVersion

---
+++ test/uk/gov/hmrc/rasapi/models/ApiVersionSpec.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.rasapi.models
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class ApiVersionSpec extends AnyWordSpec with Matchers {
+
+  "V1_0" must {
+    "return 'v1' as string" in {
+      V1_0.toString mustEqual "v1"
+    }
+  }
+
+  "V2_0" must {
+    "return 'v2' as string" in {
+      V2_0.toString mustEqual "v2"
+    }
+  }
+
+}
